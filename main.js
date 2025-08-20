e5547afe1e1196d4fb71c7dd10bbdb9f27af392b
ismael1222 <holgaloper@gmail.com>
2025-08-20 17:33:29 -0300

    Looking forward to it, roadmap ready

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
 newElementsP = document.getElementById("newElements");
 exitedElementsP = document.getElementById("exitedElements");
 counterElement = document.getElementById("counter")
-var oldElements;
+var oldElements = [];
 var counter = 0;
 function hasAthatBdoesnt(A, B) {
     var result = [];
-
     A.forEach(item => {
-        if (item.startsWith("kworker")) 
-            return;
-        
-
-
         if (! B.includes(item)) {
             result.push(item)
         }
     })
     return result;
 }
-
-
-queryProcesses().then(currentElements => {
-    oldElements = currentElements;
-})
 setInterval(() => {
     queryProcesses().then(currentElements => {
         let newItems = hasAthatBdoesnt(currentElements, oldElements); // update new elements
-        newElementsP.innerHTML = newItems; // increment counter
+        newElementsP.innerHTML = newItems // increment counter
         counterElement.innerHTML = ++ counter // update old elements
         let exitedElements = hasAthatBdoesnt(oldElements, currentElements);
         exitedElementsP.innerHTML = exitedElements; // newelements get old
